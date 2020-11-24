78c784c16ff7fa07ad2315a936bb510c9a64167a
amirtabatabaei69 <45861382+amirtabatabaei69@users.noreply.github.com>
2020-11-24 10:36:12 +0000

    commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -56,6 +56,8 @@ $(".carousel-container").slick({
     "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
   nextArrow:
     "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+  autoplay: true,
+  autoplaySpeed: 4000,
 });
 
 // 3D ANIMATION
