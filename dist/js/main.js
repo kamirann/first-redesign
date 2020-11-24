a94bfe4dd427bd0a085d7748ed75cc39a9bb86a1
kamranparmaci <68832378+kamranparmaci@users.noreply.github.com>
2020-11-24 14:06:24 +0330

    some changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -56,6 +56,7 @@ $(".carousel-container").slick({
     "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
   nextArrow:
     "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+  autoplay: true,
 });
 
 // 3D ANIMATION
